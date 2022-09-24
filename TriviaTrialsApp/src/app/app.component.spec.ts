6eec43fbba9ab53f20e9f1163124572673ea5bf6
unknown <32639853+mrm6068@users.noreply.github.com>
2022-09-24 15:23:36 -0400

    1- Unit tests for name input with validation

--- TriviaTrialsApp/src/app/app.component.spec.ts
+++ TriviaTrialsApp/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
@@ -6,6 +7,7 @@ import { StartScreenComponent } from './start-screen/start-screen.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [
         AppComponent,
         StartScreenComponent
@@ -27,11 +29,22 @@ describe('AppComponent', () => {
 
   it('Should start game when "Single Player Game" button is clicked', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    // const app = fixture.componentInstance;
+    // fixture.debugElement.componentInstance.playerName = "Tester";
+    // app.playerName = 'Tester';
     fixture.detectChanges();
 
     // Start screen is present in DOM
     var startScreenDebugElement = fixture.debugElement.query(By.css('start-screen'));
     expect(startScreenDebugElement).toBeTruthy();
+
+    // const playerNameElement = fixture.debugElement.query(By.css('.name-input')).nativeElement.value = 'Tester';
+    // fixture.debugElement.query(By.css('.name-input')).nativeElement.value = 'Tester';
+    let input = fixture.debugElement.query(By.css('.name-input'));
+    let el = input.nativeElement;
+    el.value = 'Tester';
+    el.dispatchEvent(new Event('input'));
+    // playerNameElement.
 
     // Click single player button
     const buttonElement = fixture.debugElement.query(By.css('.single-player-button'));
--- TriviaTrialsApp/src/app/start-screen/start-screen.component.spec.ts
+++ TriviaTrialsApp/src/app/start-screen/start-screen.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { StartScreenComponent } from './start-screen.component';
 
 describe('StartScreenComponent', () => {
@@ -20,4 +20,65 @@ describe('StartScreenComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit event when "Single Player Game" is clicked and a name provided', () => {
+    const fixture = TestBed.createComponent(StartScreenComponent);
+    // spy on event emitter
+    const component = fixture.componentInstance;
+    spyOn(component.newSinglePlayerGameEvent, 'emit');
+
+    // PLayer adds name
+    component.playerName = 'Tester';
+
+    // Click Single Player button
+    const nativeElement = fixture.nativeElement;
+    const button = nativeElement.querySelector('.single-player-button');
+    button.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+
+    // Event has been emitted with the player's name
+    expect(component.newSinglePlayerGameEvent.emit).toHaveBeenCalledWith('Tester');
+ });
+
+ it('should not emit event when "Single Player Game" is clicked and a name not provided', () => {
+  const fixture = TestBed.createComponent(StartScreenComponent);
+  // spy on event emitter
+  const component = fixture.componentInstance;
+  spyOn(component.newSinglePlayerGameEvent, 'emit');
+
+  // trigger the click
+  const nativeElement = fixture.nativeElement;
+  const button = nativeElement.querySelector('.single-player-button');
+  button.dispatchEvent(new Event('click'));
+
+  fixture.detectChanges();
+
+  // No Event emitted since player hasn't entered name
+  expect(component.newSinglePlayerGameEvent.emit).toHaveBeenCalledTimes(0);
+});
+
+it('should show red border on name input when game initiated but no name provided unitl user provides name', () => {
+  const fixture = TestBed.createComponent(StartScreenComponent);
+  const component = fixture.componentInstance;
+
+  // Initiate game without entering name
+  const nativeElement = fixture.nativeElement;
+  const button = nativeElement.querySelector('.single-player-button');
+  button.dispatchEvent(new Event('click'));
+  fixture.detectChanges();
+
+  let input = fixture.debugElement.query(By.css('.name-input'));
+  let input_el = input.nativeElement;
+
+  // Red border since player didn't enter name and initiated a game
+  expect(input_el.classList.contains('red-border')).toBeTruthy()
+
+  // Player enters name
+  component.playerName = 'Tester';
+  fixture.detectChanges();
+
+  // No more red border since player entered name
+  expect(input_el.classList.contains('red-border')).toBeFalsy()
+});
+
 });
