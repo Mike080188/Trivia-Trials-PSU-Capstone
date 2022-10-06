a33d9cc9e445b8ec89748dbbdd71d6949538cd23
unknown <32639853+mrm6068@users.noreply.github.com>
2022-10-06 00:43:50 -0400

    2-Changing lambda response

--- lambdas/start_game/start_game.py
+++ lambdas/start_game/start_game.py
@@ -11,6 +11,9 @@ def handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello World5555!!")
+        "body": json.dumps("Hello World5555!!"),
+        # 'headers': {
+        #     'Content-Type': 'application/json',
+        # },
     }
     # return "Hello World!!"
