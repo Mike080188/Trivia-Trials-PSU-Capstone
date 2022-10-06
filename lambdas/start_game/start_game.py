9e9547f96fd1341539a7efd908fe8855cf4e6eb4
unknown <32639853+mrm6068@users.noreply.github.com>
2022-10-06 18:06:18 -0400

    3-test returning questions

--- lambdas/start_game/start_game.py
+++ lambdas/start_game/start_game.py
@@ -8,10 +8,26 @@ def handler(event, context):
     # status_code = r.status_code
 
     # return_statement = "Google status code: " + status_code + "\nPandas version: " + p_version
+    body = [
+        {
+            'q': 'Who killed Alexander Hamilton?',
+            'correctAnswer': 'Vice President Aaaron Burr',
+            'wrongAnswer1': 'Patrick Henry',
+            'wrongAnswer2': 'Himself',
+            'wrongAnswer3': 'Benedict Arnold',
+        },
+        {
+            'q': 'What is 37 x 4 x 5?',
+            'correctAnswer': '740',
+            'wrongAnswer1': '620',
+            'wrongAnswer2': '988',
+            'wrongAnswer3': '580',
+        }
+    ]
 
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello World5555!!"),
+        "body": json.dumps(body),
         # 'headers': {
         #     'Content-Type': 'application/json',
         # },
