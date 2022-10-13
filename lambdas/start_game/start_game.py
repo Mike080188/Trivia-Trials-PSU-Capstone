ec53e6a83cc2efcff6d10ebbb9e06e6e45e86fdd
unknown <32639853+mrm6068@users.noreply.github.com>
2022-10-13 02:46:54 -0400

    3 - working on api errors

--- lambdas/start_game/start_game.py
+++ lambdas/start_game/start_game.py
@@ -5,13 +5,79 @@ import dynamo_util
 
 def handler(event, context):
 
-    questions = dynamo_util.get_random_questions(4)
+    body = [
+        {
+            'q': 'Who killed Alexander Hamilton?',
+            'answers': [
+                {
+                    'answer': 'Vice President Aaaron Burr',
+                    'isCorrect': True
+                },
+                {
+                    'answer': 'Patrick Henry',
+                    'isCorrect': False
+                },
+                {
+                    'answer': 'Himself',
+                    'isCorrect': False
+                },
+                {
+                    'answer': 'Benedict Arnold',
+                    'isCorrect': False
+                }
+            ]
+        },
+        {
+            'q': 'What is 37 x 4 x 5?',
+            'answers': [
+                {
+                    'answer': '740',
+                    'isCorrect': True
+                },
+                {
+                    'answer': '620',
+                    'isCorrect': False
+                },
+                {
+                    'answer': '988',
+                    'isCorrect': False
+                },
+                {
+                    'answer': '580',
+                    'isCorrect': False
+                }
+            ]
+        },
+        {
+            'q': 'What year was Jurassic Park released?',
+            'answers': [
+                {
+                    'answer': '1997',
+                    'isCorrect': False
+                },
+                {
+                    'answer': '1995',
+                    'isCorrect': False
+                },
+                {
+                    'answer': '1993',
+                    'isCorrect': True
+                },
+                {
+                    'answer': '1991',
+                    'isCorrect': False
+                }
+            ]
+        }
+    ]
+    questions = dynamo_util.get_random_questions(3)
 
     return {
         "statusCode": 200,
-        "body": json.dumps(questions),
+        "body": json.dumps(body),
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
         },
     }
+    # return "Hello World!!"
