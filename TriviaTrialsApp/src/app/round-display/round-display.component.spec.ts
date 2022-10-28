58b108261ed438698883c2b0f20b7c62d9bef2e2
unknown <32639853+mrm6068@users.noreply.github.com>
2022-10-28 00:18:53 -0400

    59- Test display current round during game

--- TriviaTrialsApp/src/app/round-display/round-display.component.spec.ts
+++ TriviaTrialsApp/src/app/round-display/round-display.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { RoundDisplayComponent } from './round-display.component';
 
@@ -21,5 +22,26 @@ describe('RoundDisplayComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should display round number during the game', async () => {
+    const fixture = TestBed.createComponent(RoundDisplayComponent);
+
+    const component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    spyOn(component.gameService, "loadQuestions").and.stub(); // don't call api
+
+    component.gameService.round = 4;
+    component.gameService.gameOn = true;
+
+    // Start game with name 'Tester'
+    // component.startSinglePlayerGame('Tester');
+    fixture.detectChanges();
+
+    // Round was set to four, so should display 'Round: 4'
+    var roundElement = fixture.debugElement.query(By.css('.round'));
+    var roundText = roundElement.nativeElement.textContent
+    expect(roundText.trim()).toEqual("Round: 4");
   });
 });
