4e4e813650916d47f707dfbc610f5367457e66b5
unknown <32639853+mrm6068@users.noreply.github.com>
2022-11-12 01:49:04 -0500

    6-Fix logger issues

--- lambdas/save_score/save_score.py
+++ lambdas/save_score/save_score.py
@@ -4,7 +4,8 @@ import boto3
 from dynamo_util import save_score
 import logging
 
-logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(message)s')
+logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
 def handler(event, context):
