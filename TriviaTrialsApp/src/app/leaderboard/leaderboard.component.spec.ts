b5f09abf3e33d24073c3aa50b017bba66a3174b2
unknown <32639853+mrm6068@users.noreply.github.com>
2022-11-30 20:22:50 -0500

    6 - Unit tests for finding if player made leaderboard

--- TriviaTrialsApp/src/app/leaderboard/leaderboard.component.spec.ts
+++ TriviaTrialsApp/src/app/leaderboard/leaderboard.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { GameScore } from 'app/model/game_score';
 
 import { LeaderboardComponent } from './leaderboard.component';
 
@@ -22,4 +23,35 @@ describe('LeaderboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return correct header for making the leaderboard', () => {
+    component.gameService.player = new GameScore()
+    component.gameService.player.name = "Tester"
+    component.gameService.player.score = 100
+
+    // Add player to leaderboard
+    component.gameService.leaderboard.push(component.gameService.player)
+
+    let response = component.getLeaderboardHeader()
+
+    expect(response).toEqual("Top Score!");
+  });
+
+  it('should return correct header for NOT making the leaderboard', () => {
+    let testScore = new GameScore()
+    testScore.name = "Tester"
+    testScore.score = 100
+
+    component.gameService.player = new GameScore()
+    component.gameService.player.name = "Tester_2"
+    component.gameService.player.score = 200
+
+    // Add non-player score to leaderboard
+    component.gameService.leaderboard.push(testScore)
+
+    let response = component.getLeaderboardHeader()
+
+    expect(response).toEqual("Leaderboard");
+  });
+
 });
